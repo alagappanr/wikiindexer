0c40d89ebe9f6221e4dffce43eef1553dbeb9d38
Palaniappan Meiyappan <palaniap@buffalo.edu>
2013-09-26 03:12:50 -0400

    Stax parsing code complete

--- src/edu/buffalo/cse/ir/wikiindexer/parsers/Parser.java
+++ src/edu/buffalo/cse/ir/wikiindexer/parsers/Parser.java
@@ -3,19 +3,30 @@
  */
 package edu.buffalo.cse.ir.wikiindexer.parsers;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Properties;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 import edu.buffalo.cse.ir.wikiindexer.wikipedia.WikipediaDocument;
 
 /**
  * @author nikhillo
- *
+ * 
  */
 public class Parser {
 	/* */
 	private final Properties props;
-	
+	Collection<WikipediaDocument> documents;
+
 	/**
 	 * 
 	 * @param idxConfig
@@ -24,7 +35,7 @@ public class Parser {
 	public Parser(Properties idxProps) {
 		props = idxProps;
 	}
-	
+
 	/* TODO: Implement this method */
 	/**
 	 * 
@@ -32,18 +43,127 @@ public class Parser {
 	 * @param docs
 	 */
 	public void parse(String filename, Collection<WikipediaDocument> docs) {
+
+		// String rootDir = props.getProperty("root.dir");
+		// String fileName = props.getProperty("dump.filename");
+		// String xmlFile = rootDir + fileName;
 		
+		StringBuffer textContent = new StringBuffer();
+		String content = null;
+		Boolean istextContent = false;
+		Boolean isheaderContent = false;
+		String title = null;
+		int id = 0;
+		String timeStamp = null;
+		String author = null;
+
+		
+		try {
+			
+			if(filename == null || filename.isEmpty()) {
+				throw new Exception("XML Filename not passed for parsing. Unable to proceed.");
+			} 
+			System.out.println(filename);
+			FileInputStream fis = new FileInputStream(filename);
+			XMLInputFactory factory = XMLInputFactory.newInstance();
+			XMLStreamReader reader = factory.createXMLStreamReader(filename,
+					fis);
+			while (reader.hasNext()) {
+				int constants = reader.next();
+
+				switch (constants) {
+				case XMLStreamConstants.START_ELEMENT:
+					// System.out.println("reader.getLocalName() : "+
+					// reader.getLocalName());
+					switch (reader.getLocalName()) {
+					case "page":
+						// System.out.println("page entered");
+						isheaderContent = true;
+						break;
+					case "text":
+						System.out.println("text");
+						istextContent = true;
+						break;
+					}
+					break;
+				case XMLStreamConstants.CHARACTERS:
+					// System.out.println("characters");
+					if (istextContent) {
+						textContent.append(reader.getText().trim());
+						// System.out.println(textContent);
+					} else {
+						content = reader.getText().trim();
+					}
+					break;
+				case XMLStreamConstants.END_ELEMENT:
+					// System.out.println("reader.getLocalName() : "+
+					// reader.getLocalName());
+					switch (reader.getLocalName()) {
+					case "title":
+						title = content;
+						System.out.println("title : " + title);
+						break;
+					case "id":
+						if (isheaderContent) {
+							id = Integer.parseInt(content);
+							isheaderContent = false;
+						}
+						// System.out.println("id : " + id);
+						break;
+					case "timestamp":
+						timeStamp = content;
+						// System.out.println("timestamp : " + timeStamp);
+						break;
+					case "username":
+						author = content;
+						// System.out.println("username : " + author);
+						break;
+					case "text":
+						// System.out.println("text");
+						System.out.println("content : " + textContent);
+						System.out
+								.println("---------------------------------------------------------------------------------------------------------");
+						istextContent = false;
+						break;
+					case "page":
+						// System.out.println("page ");
+						WikipediaDocument singleDoc = new WikipediaDocument(id,
+								timeStamp, author, title);
+						// System.out.println("doc : "+ doc);
+						add(singleDoc, docs);
+						break;
+					}
+					break;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	/**
-	 * Method to add the given document to the collection.
-	 * PLEASE USE THIS METHOD TO POPULATE THE COLLECTION AS YOU PARSE DOCUMENTS
-	 * For better performance, add the document to the collection only after
-	 * you have completely populated it, i.e., parsing is complete for that document.
-	 * @param doc: The WikipediaDocument to be added
-	 * @param documents: The collection of WikipediaDocuments to be added to
+	 * Method to add the given document to the collection. PLEASE USE THIS
+	 * METHOD TO POPULATE THE COLLECTION AS YOU PARSE DOCUMENTS For better
+	 * performance, add the document to the collection only after you have
+	 * completely populated it, i.e., parsing is complete for that document.
+	 * 
+	 * @param doc
+	 *            : The WikipediaDocument to be added
+	 * @param documents
+	 *            : The collection of WikipediaDocuments to be added to
 	 */
-	private synchronized void add(WikipediaDocument doc, Collection<WikipediaDocument> documents) {
+	private synchronized void add(WikipediaDocument doc,
+			Collection<WikipediaDocument> documents) {
 		documents.add(doc);
 	}
 }
