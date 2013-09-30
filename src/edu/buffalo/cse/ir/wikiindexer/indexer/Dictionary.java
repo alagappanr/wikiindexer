507796634db61f9eac3ec8b381e9dcd7a63b4726
Palaniappan Meiyappan <palaniap@buffalo.edu>
2013-09-30 15:55:14 -0400

    Completed few more methods of dictionary

--- src/edu/buffalo/cse/ir/wikiindexer/indexer/Dictionary.java
+++ src/edu/buffalo/cse/ir/wikiindexer/indexer/Dictionary.java
@@ -3,37 +3,43 @@
  */
 package edu.buffalo.cse.ir.wikiindexer.indexer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * @author nikhillo
- * An abstract class that represents a dictionary object for a given index
+ * @author nikhillo An abstract class that represents a dictionary object for a
+ *         given index
  */
 public abstract class Dictionary implements Writeable {
-	
-	protected Map<String, Integer> dict[]; 
+
+	protected Map<String, Integer> dict[];
 	protected int activatedDict;
-	
-	public Dictionary (Properties props, INDEXFIELD field) {
-		
+
+	public Dictionary(Properties props, INDEXFIELD field) {
+
 		INDEXFIELD allIndexFields[] = INDEXFIELD.values();
 		dict = new HashMap[allIndexFields.length];
-		//System.out.println(allIndexFields.length);
 		int count = 0;
-		for(INDEXFIELD eachField: allIndexFields) {
-			if(field.equals(eachField)){
-				//System.out.println("Found match :: " + eachField+ " at " + activatedDict);
+		for (INDEXFIELD eachField : allIndexFields) {
+			if (field.equals(eachField)) {
 				dict[count] = new HashMap<String, Integer>();
 				activatedDict = count;
 			}
 			count++;
 		}
- 	}
-	
-	/* (non-Javadoc)
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see edu.buffalo.cse.ir.wikiindexer.indexer.Writeable#writeToDisk()
 	 */
 	public void writeToDisk() throws IndexerException {
@@ -41,43 +47,78 @@ public abstract class Dictionary implements Writeable {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see edu.buffalo.cse.ir.wikiindexer.indexer.Writeable#cleanUp()
 	 */
 	public void cleanUp() {
 		// TODO Implement this method
 
 	}
-	
+
 	/**
-	 * Method to check if the given value exists in the dictionary or not
-	 * Unlike the subclassed lookup methods, it only checks if the value exists
-	 * and does not change the underlying data structure
-	 * @param value: The value to be looked up
+	 * Method to check if the given value exists in the dictionary or not Unlike
+	 * the subclassed lookup methods, it only checks if the value exists and
+	 * does not change the underlying data structure
+	 * 
+	 * @param value
+	 *            : The value to be looked up
 	 * @return true if found, false otherwise
 	 */
 	public boolean exists(String value) {
-		if(dict[activatedDict].containsKey(value))
+		if (dict[activatedDict].containsKey(value))
 			return true;
 		else
 			return false;
 	}
-	
+
 	/**
-	 * MEthod to lookup a given string from the dictionary.
-	 * The query string can be an exact match or have wild cards (* and ?)
-	 * Must be implemented ONLY AS A BONUS
-	 * @param queryStr: The query string to be searched
+	 * MEthod to lookup a given string from the dictionary. The query string can
+	 * be an exact match or have wild cards (* and ?) Must be implemented ONLY
+	 * AS A BONUS
+	 * 
+	 * @param queryStr
+	 *            : The query string to be searched
 	 * @return A collection of ordered strings enumerating all matches if found
-	 * null if no match is found
+	 *         null if no match is found
 	 */
 	public Collection<String> query(String queryStr) {
-		//TODO: Implement this method (FOR A BONUS)
-		return null;
+		int count = 0;
+		String key;
+		List<String> matches = new ArrayList<String>();
+		try {
+			Set<String> keys = dict[activatedDict].keySet();
+			if (queryStr.contains("*")) {
+				queryStr = queryStr.replaceAll("[\\*]", "[A-Za-z0-9]*");
+			} else if (queryStr.contains("?")) {
+				queryStr = queryStr.replaceAll("[\\?]", "[A-Za-z0-9]");
+			}			
+			Pattern p = Pattern.compile("(^" + queryStr + "$)");
+			Matcher m;
+			Iterator<String> ite = keys.iterator();
+
+			while (ite.hasNext()) {
+				key = ite.next();
+				m = p.matcher(key);
+				if (m.matches() && m.group() != null) {
+					matches.add(key);
+					count++;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (count == 0)
+			return null;
+		else
+			return matches;
 	}
-	
+
 	/**
 	 * Method to get the total number of terms in the dictionary
+	 * 
 	 * @return The size of the dictionary
 	 */
 	public int getTotalTerms() {
