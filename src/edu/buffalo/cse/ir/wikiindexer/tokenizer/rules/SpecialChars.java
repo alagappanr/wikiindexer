f1e7c79b6b8f284a851dcacd7bd21f9a13a7a64f
alagappanr <ralagappan2006@gmail.com>
2013-10-05 19:14:15 -0400

    Fixed BUG

--- src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/SpecialChars.java
+++ src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/SpecialChars.java
@@ -39,16 +39,24 @@ public class SpecialChars implements TokenizerRule {
 				if (token != null) {
 					stream.previous(); //move token back as we need to change last read token
 					boolean split = splitToken(token);
+//					System.out.println("To split or not to split");
 					if(!split){
-//						System.out.println("No split");
+//						System.out.println("NO split");
 						token = RemoveSpecialChars(token);
-						stream.set(token);	// change value
+//						System.out.println("|"+token+"|");
+						if(token.isEmpty()){
+							stream.remove();
+						} else {
+							stream.set(token);		
+						}
+						// change value
 						stream.next();					// move iter to next position, beyond the token we just changed
 //						System.out.println("Set just string");
 					}
 					else{
 //						System.out.println("Split");
 						String[] token_array = RemoveSpecialCharsArray(token);
+//						System.out.println(token_array.length);
 						if(token_array.length!=0){
 							for(int i=0;i<token_array.length;i++){
 //								System.out.println(token_array[i]);
@@ -71,12 +79,19 @@ public class SpecialChars implements TokenizerRule {
 								}
 								
 								String to = ta.toString().trim().replaceAll(" +", " ");
-//								System.out.println(to);
+//								System.out.println("YO"+to+"YO");
 //								stream.set(token_array);
-								stream.set(to);
+								if(to.isEmpty()){
+									stream.remove();
+								} else {
+									stream.set(to);
+								}
+//								stream.previous();
+//								String current = stream.next();
+//								System.out.println("CUR"+current+"CUR");
 //								System.out.println("Midway"+stream.getFullTokenStream());
 								
-								stream.mergeWithPrevious();
+//								stream.mergeWithPrevious();
 //								System.out.println("After"+stream.getFullTokenStream());
 							} else {
 //							System.out.println(stream.getFullTokenStream());
@@ -97,16 +112,22 @@ public class SpecialChars implements TokenizerRule {
 //						stream.remove(); //move not required because the remove method automagically does this for us.
 //					}
 				}
-//				System.out.println(stream.getFullTokenStream());
 			}
 
 		}
 //		System.out.println("Exit stream size++++"+stream.getSize());
+//		System.out.println(stream.getFullTokenStream());
 		stream.reset();
+//		String token2;
+//		while (stream.hasNext()) { 
+//			token2 = stream.next(); 
+//		    System.out.println("****"+token2+"***");
+//		}
+//		stream.reset();
 	}
 	 
 	public boolean splitToken(String token){		
-//		System.out.println(token);
+//		System.out.println("CHECKING"+token);
 		if (token.matches("(.*?)[^\\dA-Za-z\\-\\. ](.*)")) {
 //			System.out.println("Matches pattern -> "+token);
 //			System.out.println("True");
@@ -117,7 +138,6 @@ public class SpecialChars implements TokenizerRule {
 	public String RemoveSpecialChars(String token){
 //		System.out.println("Received token->|"+token+"|");
 		token = token.replaceAll("[^\\dA-Za-z\\-\\. ]", ""); 
-//		System.out.println("Normalized->|"+token+"|");
 //		System.out.println("Returning->|"+token+"|");
 		return token;
 	}
@@ -133,13 +153,19 @@ public class SpecialChars implements TokenizerRule {
 		String[] token_array = token.split("[^\\dA-Za-z\\-\\. ]"); 
 //		System.out.println("Normalized->|"+token+"|");
 //		System.out.println("Returning->|"+token_array.length+"|");
-		String test_string = new String();
-		for(int i=0;i<token_array.length;i++){
-//			test_string = new String();
-			test_string.concat(token_array[i]);
-//			System.out.println(test_string+" after appending |"+token_array[i]+"|");
-		}
-//		System.out.println(test_string+"|at the end");
+//		String test_string = new String();
+//		for(int i=0;i<token_array.length;i++){
+////			test_string = new String();
+////			token_array[i] = token_array[i].replaceAll(" +", "");
+//			test_string.concat(token_array[i]);
+////			System.out.println(test_string+" after appending |"+token_array[i]+"|");
+//		}
+////		System.out.println("|"+test_string+"|at the end");
+//		if(token_array.length==2){
+//			System.out.println("----"+token_array[0]+"-----"+token_array[1]+"----");
+//			System.out.println("|"+token_array[1].trim()+"|");
+//		}
+		
 		return token_array;
 	}
 
