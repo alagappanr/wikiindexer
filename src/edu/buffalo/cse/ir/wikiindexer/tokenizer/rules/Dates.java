c6fc9bb25c5624ef09b4e1e1ed6ece77ec4728a6
alagappanr <ralagappan2006@gmail.com>
2013-10-05 23:46:10 -0400

    removed date

--- src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/Dates.java
+++ src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/Dates.java
@@ -29,102 +29,102 @@ public class Dates implements TokenizerRule {
 	 * @see edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenizerRule#apply(edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenStream)
 	 */
 	public void apply(TokenStream stream) throws TokenizerException {
-		String[] regex_patterns = {
-				"\\d{2,4}(\\s|\\-|/|,)\\d{2}(\\s|\\-|/|,)\\d{2}",
-				"\\d{2}/\\d{2}/\\d{2}",
-				"\\d{2}/\\d{2}/\\d{2,4}",
-				"\\d{2}-\\d{2}-\\d{2,4}",
-				"\\d{4} [a-zA-Z]{3,} \\d{2}",
-				"\\d{4} \\d{2} [a-zA-Z]{3,}",
-				"\\d{1,2} [a-zA-Z]{3,} \\d{4}",
-				"[a-zA-Z]{3,}(\\s|\\-|/|,)+\\d{1,2}(\\s|\\-|/|,)+\\d{4}",
-				"\\d{1,4} [a-zA-Z]{3,}",
-				"[a-zA-Z]{3,} \\d{2} \\d{4}",
-				"[a-zA-Z]{3,} \\d{2}",
-				"\\d{2,4} [AD|BC]{2}",
-				"\\d{4}"};
-		
-		if (stream != null) {
-			String full_stream, full_text;
-			if(stream.getSize()!=1){
-//				System.out.println("Size not 1");
-				full_stream = stream.getFullTokenStream();
-				for(String pattern: regex_patterns){
-					Pattern date_pattern = Pattern.compile(pattern);
-					Matcher date_matcher = date_pattern.matcher(full_stream);
-					boolean m = date_matcher.find();
-					String s=null;
-					String r=null;
-					int i=0;
-					while(m){
-//						System.out.println("Found!");
-						s = date_matcher.group(0);
-						//date_matcher.replaceAll("$1");
-						r = ConvertDate(s);
-						//r.append(date_matcher.group(0).replaceAll("\\-", " "));
-					//	i+=1;
-						m = date_matcher.find();
-					}
-//					System.out.println(s);
-//					System.out.println(r);
-				}
-//				full_stream = full_stream.replace("[", "");
-//				full_stream = full_stream.replace("]", "");
-//				full_stream = full_stream.replace(", "," ");
-//				String[] newArr = full_stream.split(" ");
-//				for( int i = 0; i < newArr.length; i++)
-//				{
-//				    stream.append(newArr[i]);
+//		String[] regex_patterns = {
+//				"\\d{2,4}(\\s|\\-|/|,)\\d{2}(\\s|\\-|/|,)\\d{2}",
+//				"\\d{2}/\\d{2}/\\d{2}",
+//				"\\d{2}/\\d{2}/\\d{2,4}",
+//				"\\d{2}-\\d{2}-\\d{2,4}",
+//				"\\d{4} [a-zA-Z]{3,} \\d{2}",
+//				"\\d{4} \\d{2} [a-zA-Z]{3,}",
+//				"\\d{1,2} [a-zA-Z]{3,} \\d{4}",
+//				"[a-zA-Z]{3,}(\\s|\\-|/|,)+\\d{1,2}(\\s|\\-|/|,)+\\d{4}",
+//				"\\d{1,4} [a-zA-Z]{3,}",
+//				"[a-zA-Z]{3,} \\d{2} \\d{4}",
+//				"[a-zA-Z]{3,} \\d{2}",
+//				"\\d{2,4} [AD|BC]{2}",
+//				"\\d{4}"};
+//		
+//		if (stream != null) {
+//			String full_stream, full_text;
+//			if(stream.getSize()!=1){
+////				System.out.println("Size not 1");
+//				full_stream = stream.getFullTokenStream();
+//				for(String pattern: regex_patterns){
+//					Pattern date_pattern = Pattern.compile(pattern);
+//					Matcher date_matcher = date_pattern.matcher(full_stream);
+//					boolean m = date_matcher.find();
+//					String s=null;
+//					String r=null;
+//					int i=0;
+//					while(m){
+////						System.out.println("Found!");
+//						s = date_matcher.group(0);
+//						//date_matcher.replaceAll("$1");
+//						r = ConvertDate(s);
+//						//r.append(date_matcher.group(0).replaceAll("\\-", " "));
+//					//	i+=1;
+//						m = date_matcher.find();
+//					}
+////					System.out.println(s);
+////					System.out.println(r);
 //				}
-				stream.set(full_stream);
-				stream.next();
-			} else {
-				full_text = stream.next();
-				String s=null;
-				String r=null;
-				boolean forBreak = false;
-//				System.out.println("Size=1");
-				for(String pattern: regex_patterns){
-					Pattern date_pattern = Pattern.compile(pattern);
-					System.out.println(pattern);
-					Matcher date_matcher = date_pattern.matcher(full_text);
-					boolean m = date_matcher.find();
-					int i=0;
-					while(m){
-//						System.out.println("Found!");
-//						System.out.println(pattern);
-						s = date_matcher.group(0);
-//						System.out.println(s);
-						//date_matcher.replaceAll("$1");
-						r = ConvertDate(s);
-						//r.append(date_matcher.group(0).replaceAll("\\-", " "));
-					//	i+=1;
-						if(s.contains("BC")){
-							full_text = full_text.replace(s, "-"+r);
-						} else {
-							full_text = full_text.replace(s, r);
-						}
-						forBreak = true;
-						m = date_matcher.find();
-					}
-					if(forBreak==true){
-						break;
-					}
-				}
-				
-//				System.out.println(s);
-//				System.out.println(r);
-//				System.out.println(full_text);
-				
-//				System.out.println(stream.getFullTokenStream());
-				stream.removeAll();
-//				System.out.println(stream.getFullTokenStream());
-				stream.append(full_text);
-//				System.out.println(stream.getFullTokenStream());
-			}
-			
-			stream.reset();
-		}
+////				full_stream = full_stream.replace("[", "");
+////				full_stream = full_stream.replace("]", "");
+////				full_stream = full_stream.replace(", "," ");
+////				String[] newArr = full_stream.split(" ");
+////				for( int i = 0; i < newArr.length; i++)
+////				{
+////				    stream.append(newArr[i]);
+////				}
+//				stream.set(full_stream);
+//				stream.next();
+//			} else {
+//				full_text = stream.next();
+//				String s=null;
+//				String r=null;
+//				boolean forBreak = false;
+////				System.out.println("Size=1");
+//				for(String pattern: regex_patterns){
+//					Pattern date_pattern = Pattern.compile(pattern);
+//					System.out.println(pattern);
+//					Matcher date_matcher = date_pattern.matcher(full_text);
+//					boolean m = date_matcher.find();
+//					int i=0;
+//					while(m){
+////						System.out.println("Found!");
+////						System.out.println(pattern);
+//						s = date_matcher.group(0);
+////						System.out.println(s);
+//						//date_matcher.replaceAll("$1");
+//						r = ConvertDate(s);
+//						//r.append(date_matcher.group(0).replaceAll("\\-", " "));
+//					//	i+=1;
+//						if(s.contains("BC")){
+//							full_text = full_text.replace(s, "-"+r);
+//						} else {
+//							full_text = full_text.replace(s, r);
+//						}
+//						forBreak = true;
+//						m = date_matcher.find();
+//					}
+//					if(forBreak==true){
+//						break;
+//					}
+//				}
+//				
+////				System.out.println(s);
+////				System.out.println(r);
+////				System.out.println(full_text);
+//				
+////				System.out.println(stream.getFullTokenStream());
+//				stream.removeAll();
+////				System.out.println(stream.getFullTokenStream());
+//				stream.append(full_text);
+////				System.out.println(stream.getFullTokenStream());
+//			}
+//			
+//			stream.reset();
+//		}
 
 	}
 	@SuppressWarnings("deprecation")
